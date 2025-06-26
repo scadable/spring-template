9187e2b7e65356b1dddff4d8f760d3f4decf2201
Ali Rahbar <alirahbar2005@gmail.com>
2025-06-25 23:41:48 -0400

    added initial project

---
+++ src/main/java/main/scadable/livequery/LiveQueryApplication.java
@@ -0,0 +1,15 @@
+package main.scadable.livequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveQueryApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LiveQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/scadable/livequery/LiveQueryApplicationTests.java
@@ -0,0 +1,15 @@
+package main.scadable.livequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveQueryApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
